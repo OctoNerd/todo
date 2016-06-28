71db283989a2faa51f25f529aae7d548c583e309
Nick Eddy <nick.a.eddy@gmail.com>
2016-06-28 15:43:22 -0700

    added delete item functionality

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
+var i = 0;
+
 //Delete item - Slide off to the left and fade out.
+function delete_item(n) {
+	n.parentNode.parentNode.parentNode.removeChild(n.parentNode.parentNode);
+}
 
 //Edit item - Click icon and change label to a textbox which replaces old label
 
 //Priority bar color change - cycles through different priority levels
 
 //Add blank item to bottom of list
-var i = 0;
+
 
 function new_item() {
 	var blankTask = document.getElementById('original-item');
 	var clone = blankTask.cloneNode(true);
 
-	clone.id = "duplicate" + ++i;
+	clone.id = 'duplicate' + ++i;
 	blankTask.parentNode.appendChild(clone);
 
 }
