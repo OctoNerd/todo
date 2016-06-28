dc063b9ace5ad278f1bf1fd5d71044029f5b5664
Nick Eddy <nick.a.eddy@gmail.com>
2016-06-28 12:39:37 -0700

    re-arranged, outlined js tasks

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Delete item - Slide off to the left and fade out.
+
+//Edit item - Click icon and change label to a textbox which replaces old label
+
+//Priority bar color change - cycles through different priority levels
+
+//Add new list button
+
+//Add new item button
