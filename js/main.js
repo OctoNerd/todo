d807f61d23bc4ee8b84d661e4540dc1224572f0a
Nick Eddy <nick.a.eddy@gmail.com>
2016-06-28 19:33:31 -0700

    fixed add item dependency on original item

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var i = 0;
-var j = 1;
 
 //Delete item - Slide off to the left and fade out.
 function delete_item(n) {
@@ -13,32 +12,24 @@ function delete_item(n) {
 
 //Priority bar color change - cycles through different priority levels
 function change_priority(n) {
-	if (j === 3) {
-		j = 0;
-	}
-
-	if (j === 0) {
+	if (n.className === "priority-low priority-button") {
 		//replace classes with priority-low class
-		n.className = "priority-low";
+		n.className = "priority-mid";
 		//add priority-button class
 		n.className += " priority-button";
-	} else if (j === 1) {
-		n.className = "priority-mid";
-		n.className += " priority-button";
-	} else {
+	} else if (n.className === "priority-mid priority-button") {
 		n.className = "priority-high";
 		n.className += " priority-button";
+	} else {
+		n.className = "priority-low";
+		n.className += " priority-button";
 	}
-
-	j++;
-
-	console.log("j is equal to " + j);
 }
 
 //Add blank item to bottom of list
 function new_item() {
 	
-	var blankTask = document.getElementById('original-item');
+	var blankTask = document.querySelector('.item');
 	var clone = blankTask.cloneNode(true);
 
 	clone.id = 'duplicate' + ++i;
